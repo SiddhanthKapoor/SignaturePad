173805f5bd02003c05b5be4feca5e40308a9fb9f
Siddhanth Kapoor <kapoorsiddhanth15@gmail.com>
2024-09-22 06:38:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const canvas = document.getElementById('pad');
+const clearbtn = document.getElementById('clear');
+const save = document.getElementById('save');
+const cxt = canvas.getContext('2d');
+let writingmode = false ; 
+
+clearbtn.addEventListener('click',clear);
+save.addEventListener('click',saveCanvas);
+
+
+
+canvas.addEventListener('mousedown',start);
+canvas.addEventListener('touchstart', start, { passive: false });
+
+
+canvas.addEventListener('mouseup' , stop);
+canvas.addEventListener('touchend', stop);
+
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('touchmove', draw, { passive: false });
+
+function start(e){
+    writingmode = true;
+    draw(e);
+}
+
+function stop(){
+    writingmode=false;
+    cxt.beginPath();
+}
+
+function draw(e){
+  if (!writingmode) return;
+const rect = canvas.getBoundingClientRect();
+const x = (e.clientX || e.touches[0].clientX) - rect.left;
+const y = (e.clientY || e.touches[0].clientY) - rect.top;
+cxt.lineWidth = 1;
+cxt.lineCap = 'round';
+cxt.lineTo(x, y);
+cxt.stroke();
+cxt.beginPath();
+cxt.moveTo(x, y);
+}
+
+function clear() {
+    cxt.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  
+function saveCanvas() {
+    const dataURL = canvas.toDataURL('image/jpg');
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'mysignature.jpg'; 
+    link.click();
+  }
